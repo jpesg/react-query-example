a1c20e3f0416a288fc9926223abb9ca342103e0a
jpesg <42676743+jpesg@users.noreply.github.com>
2021-12-14 14:37:09 +0000

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import Post from "./Post";
+import Posts from "./Posts";
+import "./styles.css";
+
+export default function App() {
+  return (
+    <div className="App">
+      <Post />
+      <Posts />
+    </div>
+  );
+}
